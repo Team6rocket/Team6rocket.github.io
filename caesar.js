45d2d292eb978223f505eb30e8d49655c4fda3b7
Team6rocket <80188816+Team6rocket@users.noreply.github.com>
2021-03-07 17:52:08 +0530

    Update caesar.js

--- caesar.js
+++ caesar.js
@@ -1,18 +1,4 @@
-// Caesarian Shift
 
-// This code was written by Tyler Akins and is placed in the public domain.
-// It would be nice if this header remained intact.  http://rumkin.com
-
-// Requires util.js
-
-
-// Perform a Caesar cipher (ROT-N) encoding on the text
-// encdec = -1 for decode, 1 for encode (kinda silly, but kept it like this
-//    to be the same as the other encoders)
-// text = the text to encode/decode
-// inc = how far to shift the letters.
-// key = the key to alter the alphabet
-// alphabet = The alphabet to use if not A-Z
 function Caesar(encdec, text, inc, key, alphabet)
 {	
    var s = "", b, i, idx;
