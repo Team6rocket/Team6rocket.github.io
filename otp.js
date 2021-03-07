8e355bc2e7866b6d40fae2a73fc7fdaebb8cc0db
Team6rocket <80188816+Team6rocket@users.noreply.github.com>
2021-03-07 17:49:27 +0530

    Update otp.js

--- otp.js
+++ otp.js
@@ -1,14 +1,4 @@
-// One-Time Pad
 
-// This code was written by Tyler Akins and placed in the public domain.
-// It would be nice if you left this header intact.  http://rumkin.com
-
-
-// Implements a one-time pad for only alphabetic characters.  Preserves
-// the character case in the text (not the key).
-// encdec = -1 for decode, 1 for encode
-// text = the text to encode or decode.
-// key = the key (pad) to use
 function OneTimePad(encdec, text, key)
 {
    var pad, i, out, c, uc;
