c1454756b6e642b23c85289da246991a65674b68
Team6rocket <80188816+Team6rocket@users.noreply.github.com>
2021-03-07 17:53:54 +0530

    Update affine.js

--- affine.js
+++ affine.js
@@ -1,18 +1,4 @@
-// Affine Cipher
 
-// This code was written by Tyler Akins and is placed in the public domain.
-// It would be nice if this header remained intact.  http://rumkin.com
-
-// Requires util.js
-
-
-// Perform a Affine transformation on the text
-// encdec = -1 for decode, 1 for encode (kinda silly, but kept it like this
-//    to be the same as the other encoders)
-// text = the text to encode/decode
-// inc = how far to shift the letters.
-// key = the key to alter the alphabet
-// alphabet = The alphabet to use if not A-Z
 function Affine(encdec, text, mult, inc, key, alphabet)
 {
    var s = "";
